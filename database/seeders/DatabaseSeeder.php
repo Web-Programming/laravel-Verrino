0098f04908dbddb9c68e9bde74197d754acbb255
neixyuu <vickyyanto_2226250010@mhs.mdp.ac.id>
2023-11-06 13:10:35 +0700

    Menambahkan tabel prodi dan joinnya

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prodi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Prodi::create(
+            [
+                'nama' => 'Teknik Informatika'
+            ]
+        );
+        Prodi::create(
+            [
+                'nama' => 'Manajemen Informatika'
+            ]
+        );
+        Prodi::create(
+            [
+                'nama' => 'Sistem Informasi'
+            ]
+        );
     }
 }
